fdd402ac18cb24e40a1e0f038b34a43a915c2d1f
JohannaSordalen <89453144+JohannaSordalen@users.noreply.github.com>
2026-02-05 14:09:16 +0100

    Initial commit of FlyingPigProject

---
+++ _Project_/FlyingPig_2024_StartUp/Source/FlyingPigProject_00/DNA.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class FLYINGPIGPROJECT_00_API DNA
+{
+public:
+	DNA();
+	~DNA();
+};
